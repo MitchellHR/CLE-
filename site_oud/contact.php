3d8c865de9f58f45078a59e944b56e1c5db282d0
MitchellHR <0960144@hr.nl>
2020-01-06 12:08:06 +0100

    Initial commit

---
+++ site_oud/contact.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css.css">
+    </head>   
+
+<body>
+
+
+
+<?php
+include("navbar.php");
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ site_oud/navbar.php
@@ -0,0 +1,11 @@
+<head>
+        <link rel="stylesheet" href="css.css">
+    </head>  
+
+<div class = "navbar_top">
+    <div class= "navbar_item"><a href="index.php">Home</a></div>
+    <div class= "navbar_item"><a href="over_mij.php">Over mij</a></div>
+    <div class= "navbar_item"><a href="rooster.php">Rooster</a></div>
+    <div class= "navbar_item"><a href="contact.php">Contact</a></div>
+    <div class= "navbar_item"><a href ="login.php"><img class = "login_img" src = "login_img.png"></a></div>
+</div> 
